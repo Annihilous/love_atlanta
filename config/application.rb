825692231895990d679c5e412162b78c09775ad7
Annihilous <benjaminjnorton@gmail.com>
2016-05-10 11:49:07 -0400

    Revert to CORS branch.

--- config/application.rb
+++ config/application.rb
@@ -2,19 +2,20 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.middleware.insert_before 0, "Rack::Cors" do
-  allow do
-    origins '*'
-    resource '*', :headers => :any, :methods => [:get, :post, :options]
-  end
-end
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module LoveAtlanta
   class Application < Rails::Application
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
